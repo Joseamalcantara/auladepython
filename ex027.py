4d4a1b81a000c646e5572437463e1e18133e49c3
Joseamalcantara <115882540+Joseamalcantara@users.noreply.github.com>
2025-09-05 23:11:17 -0300

    atualização de rotina

--- ex027.py
+++ ex027.py
@@ -1,3 +1,8 @@
+# Faça um programa que leia o nome completo de uma pessoa, mostrando um seguida o primeiro e o último nome separadamente.
+# Ex.: Ana Maria de Souza
+# primeiro nome: Ana
+# segundo nome: Souza
+
 n = str(input('Digite seu nome completo: ')).strip()
 nome = n.split()
 print(f'Seu primeiro nome é {nome[0]}')
